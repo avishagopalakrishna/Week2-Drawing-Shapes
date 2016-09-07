29ae1ebccc091a88eaa69ab26a723f7c32aac8c0
Avisha Gopal <a.gopalakrishna@umiami.edu>
2016-09-07 09:58:50 -0400

    House Drawing Practice

---
+++ House Drawing/sketch.js
@@ -0,0 +1,146 @@
+var housepoint = 240;
+var housepoint2 = 81;
+var windowwidth = 100;
+var windowheight = 60;
+var trianglex = 0;
+var triangley = 20;
+var trianglez = 0;
+
+
+function setup() {
+
+  createCanvas(720, 400);
+  background(0);
+  noStroke();
+
+  fill(214);
+  triangle(81, 240, 210, 180, 341, 240);
+
+  fill(102);
+  rect(81, 240, 260, 260);
+  
+  fill(214);
+  rect(95,260, 100, 60);
+  
+  fill(214);
+  rect(220,260, 100, 60);
+  
+  fill(214);
+  rect(200,360, 30, 95);
+
+  fill(255);
+  ellipse(600, 94, 72, 72);
+  
+  fill(204);
+  triangle(trianglex, 400, triangley, 400, trianglez, 380);
+  
+   fill(204);
+  triangle(trianglex+20, 400, triangley+40, 400, trianglez+20, 380);
+  
+  fill(204);
+  triangle(trianglex+40, 400, triangley+60, 400, trianglez+40, 380);
+  
+    fill(204);
+  triangle(trianglex+60, 400, triangley+80, 400, trianglez+60, 380);
+  
+    fill(204);
+  triangle(trianglex+80, 400, triangley+100, 400, trianglez+80, 380);
+  
+    fill(204);
+  triangle(trianglex+100, 400, triangley+120, 400, trianglez+100, 380);
+  
+    fill(204);
+  triangle(trianglex+120, 400, triangley+140, 400, trianglez+120, 380);
+  
+    fill(204);
+  triangle(trianglex+140, 400, triangley+160, 400, trianglez+140, 380);
+  
+    fill(204);
+  triangle(trianglex+160, 400, triangley+180, 400, trianglez+160, 380);
+  
+    fill(204);
+  triangle(trianglex+180, 400, triangley+200, 400, trianglez+180, 380);
+  
+    fill(204);
+  triangle(trianglex+200, 400, triangley+220, 400, trianglez+200, 380);
+  
+    fill(204);
+  triangle(trianglex+220, 400, triangley+240, 400, trianglez+220, 380);
+  
+    fill(204);
+  triangle(trianglex+240, 400, triangley+260, 400, trianglez+240, 380);
+  
+    fill(204);
+  triangle(trianglex+260, 400, triangley+280, 400, trianglez+260, 380);
+  
+    fill(204);
+  triangle(trianglex+280, 400, triangley+300, 400, trianglez+280, 380);
+  
+    fill(204);
+  triangle(trianglex+300, 400, triangley+320, 400, trianglez+300, 380);
+  
+    fill(204);
+  triangle(trianglex+320, 400, triangley+340, 400, trianglez+320, 380);
+  
+    fill(204);
+  triangle(trianglex+340, 400, triangley+360, 400, trianglez+340, 380);
+  
+    fill(204);
+  triangle(trianglex+360, 400, triangley+380, 400, trianglez+360, 380);
+  
+    fill(204);
+  triangle(trianglex+380, 400, triangley+400, 400, trianglez+380, 380);
+  
+    fill(204);
+  triangle(trianglex+400, 400, triangley+420, 400, trianglez+400, 380);
+  
+    fill(204);
+  triangle(trianglex+420, 400, triangley+440, 400, trianglez+420, 380);
+  
+    fill(204);
+  triangle(trianglex+440, 400, triangley+460, 400, trianglez+440, 380);
+  
+    fill(204);
+  triangle(trianglex+460, 400, triangley+480, 400, trianglez+460, 380);
+  
+    fill(204);
+  triangle(trianglex+480, 400, triangley+500, 400, trianglez+480, 380);
+  
+    fill(204);
+  triangle(trianglex+500, 400, triangley+520, 400, trianglez+500, 380);
+  
+    fill(204);
+  triangle(trianglex+520, 400, triangley+540, 400, trianglez+520, 380);
+  
+    fill(204);
+  triangle(trianglex+540, 400, triangley+560, 400, trianglez+540, 380);
+  
+    fill(204);
+  triangle(trianglex+560, 400, triangley+580, 400, trianglez+560, 380);
+  
+    fill(204);
+  triangle(trianglex+580, 400, triangley+600, 400, trianglez+580, 380);
+  
+    fill(204);
+  triangle(trianglex+600, 400, triangley+620, 400, trianglez+600, 380);
+  
+    fill(204);
+  triangle(trianglex+620, 400, triangley+640, 400, trianglez+620, 380);
+  
+    fill(204);
+  triangle(trianglex+640, 400, triangley+660, 400, trianglez+640, 380);
+  
+    fill(204);
+  triangle(trianglex+660, 400, triangley+680, 400, trianglez+660, 380);
+  
+    fill(204);
+  triangle(trianglex+680, 400, triangley+700, 400, trianglez+680, 380);
+  
+    fill(204);
+  triangle(trianglex+700, 400, triangley+720, 400, trianglez+700, 380);
+
+}
+
+function draw() {
+  
+}
